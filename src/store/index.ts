a1d428b9b1aed93fdc1175940a202f0731b796eb
calamberia <calamberia@gmail.com>
2019-12-24 22:49:24 +0100

    Add redux dev tool

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 
 import app from "./app";
 
 export const rootReducer = combineReducers({ app });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export default store;
