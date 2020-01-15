070193cff3b20829a2196d8e5e50b1798b4189f2
calamberia <calamberia@gmail.com>
2020-01-15 19:38:36 +0100

    Updates

---
+++ precache-manifest.c22c9225aff4fd82dce64d885574f6d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f0f050c8a745d6ebbc41b517033f16",
+    "url": "/trip-reporter/index.html"
+  },
+  {
+    "revision": "cd91be5dc524383d5b8e",
+    "url": "/trip-reporter/static/js/2.15df0e80.chunk.js"
+  },
+  {
+    "revision": "26c31c05639d8c8e4d046eb1b98016c3",
+    "url": "/trip-reporter/static/js/2.15df0e80.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c11293fdfca396eb34b",
+    "url": "/trip-reporter/static/js/main.7f09a4d4.chunk.js"
+  },
+  {
+    "revision": "36b4e3473e0afede3eef",
+    "url": "/trip-reporter/static/js/runtime-main.6cdba916.js"
+  }
+]);
